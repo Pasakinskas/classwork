65530557f518ea9646dd54b4117cc1bf13512630
Andrius <daujotasand@gmail.com>
2023-10-27 20:22:15 +0300

    Initial ver.1

---
+++ src/main/java/lt/codeacademy/mariusp/andriusd/App.java
@@ -0,0 +1,18 @@
+package lt.codeacademy.mariusp.andriusd;
+
+import java.util.ArrayList;
+
+public class App {
+
+    public static void main(String[] args) {
+
+
+        readLines(./src/main/java/resources/vardai.txt);
+
+    }
+    public static ArrayList<String> readLines(string path){
+        try{
+            var reader =new
+        }
+    }
+}
