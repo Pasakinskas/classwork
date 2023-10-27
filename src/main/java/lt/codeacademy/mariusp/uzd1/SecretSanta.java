f5eefb55f30db3c70d18bf1c89cae24409cb85d9
Ingrida <motuziene47@gmail.com>
2023-10-27 20:39:59 +0300

    Sukurtas metodas readFiles

--- src/main/java/lt/codeacademy/mariusp/uzd1/SecretSanta.java
+++ src/main/java/lt/codeacademy/mariusp/uzd1/SecretSanta.java
@@ -1,9 +1,26 @@
 package lt.codeacademy.mariusp.uzd1;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * this will be the class that implements
  * the first task
  */
 public class SecretSanta {
+
+    public static List<String> readFiles(String fileName){
+        try {
+            var reader = new BufferedReader(new FileReader(fileName));
+            return reader.lines().collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
 
 }
