6918e83d9dff944c3af3c45dcb4b239191d8b723
Ingrida <motuziene47@gmail.com>
2023-10-27 20:55:14 +0300

    Sukurtas metodas readFiles

--- src/main/java/lt/codeacademy/ingridam/SecretSanta.java
+++ src/main/java/lt/codeacademy/ingridam/SecretSanta.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class SecretSanta {
+
     public static List<String> readFiles(String fileName){
         try {
             var reader = new BufferedReader(new FileReader(fileName));
