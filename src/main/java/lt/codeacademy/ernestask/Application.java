c761b18a829405ea363c38a89e003f471b241097
Ernestas <ernestas.kriskus@gmail.com>
2023-10-27 13:23:11 -0500

    Created reader in SecretSanta.java class and called the method in Application.java main method

--- src/main/java/lt/codeacademy/ernestask/Application.java
+++ src/main/java/lt/codeacademy/ernestask/Application.java
@@ -1,7 +1,12 @@
 package lt.codeacademy.ernestask;
 
+import java.io.IOException;
+import java.util.Random;
+
 public class Application {
-    public static void main(String[] args) {
+    Random random = new Random();
+    public static void main(String[] args) throws IOException {
+SecretSanta.readLines("./src/resources/vardai.txt");
 
     }
 }
--- src/main/java/lt/codeacademy/ernestask/SecretSanta.java
+++ src/main/java/lt/codeacademy/ernestask/SecretSanta.java
@@ -1,4 +1,20 @@
 package lt.codeacademy.ernestask;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.rmi.RemoteException;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class SecretSanta {
+
+    public static List<String> readLines(String filePath) throws IOException{
+        try {
+            var bufferedReader = new BufferedReader(new FileReader(filePath));
+            return bufferedReader.lines().collect(Collectors.toList());
+        } catch (IOException ioException){
+            throw new RuntimeException(ioException);
+        }
+    }
 }
