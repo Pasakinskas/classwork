1cf31bec58a5e5332dbeae81377b90b9ed33e9a1
Andrius <daujotasand@gmail.com>
2023-10-30 14:42:15 +0200

    Initial ver.1.2.1 cikalai isduoda 2 sarasus random skaiciu kurie neikartojas ir yra tik 12, tiek kiek yra vardu, 2 ciklai

--- src/main/java/lt/codeacademy/mariusp/andriusd/App.java
+++ src/main/java/lt/codeacademy/mariusp/andriusd/App.java
@@ -51,13 +51,14 @@ public class App {
 
         var path2 = ("./src/main/java/lt/codeacademy/mariusp/andriusd/papildytivardai.txt");
         List<String> lines = readLines(path2);
-        System.out.println("Vardu skaicius="+lines.size());
+        System.out.println("Vardu skaicius=" + lines.size());
 
         final Random random = new Random();
         final Random random2 = new Random();
 
         List<Integer> spetiSk = new ArrayList<>();
         List<Integer> spetiSk2 = new ArrayList<>();
+        //HashMap<Integer, String> spekSk1 = new HashMap<>();
 
         int varduSkaicius = lines.size() + 1;
         for (int i = 1; i < 5000; i++) {
@@ -72,7 +73,7 @@ public class App {
         }
         for (int i = 1; i < 5000; i++) {
             int randomskaicius2 = random.nextInt(varduSkaicius);
-            if (!spetiSk2.contains(randomskaicius2) && !(randomskaicius2 == 0)) {
+            if (!spetiSk2.contains(randomskaicius2) && !(randomskaicius2 == 0) && !spetiSk.equals(randomskaicius2)) {
                 spetiSk2.add(randomskaicius2);
             } else if (spetiSk2.size() == 12) {
                 break;
@@ -81,11 +82,14 @@ public class App {
         System.out.println(spetiSk);
         System.out.println(spetiSk2);
 
+        for (int i = 0; i < spetiSk.size(); i++) {
+            if (spetiSk.get(i).equals(spetiSk2.get(i))) {
 
+                System.out.println("Vienodi elementai rasti: " + spetiSk.get(i) + " pozicijoje " + (i + 1));
+
+            }
+        }
     }
-
-
-
 
 
     public static List<String> readLines(String path) {
